243e5f6a45bbf9a3033e62b5ff13aeeb27f5498d
Avi <95044844+Avioti@users.noreply.github.com>
2025-12-16 15:40:11 -0500

    skeleton issue #8

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -5,5 +5,9 @@ import org.yearup.models.ShoppingCart;
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
+    ShoppingCart getAllItemsInCart(int userId);
+    void addItemToCart(int userId, int productId, int quantity);
+    void removeItemFromCart(int userId, int productId);
+    void clearCart(int userId);
     // add additional method signatures here
 }
